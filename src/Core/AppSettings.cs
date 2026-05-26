2692b6aad91510737a1f48f94f4a8d70e106994d
Dese193 <pawelk193@hotmail.com>
2026-05-26 22:18:09 +0200

    ver

--- src/Core/AppSettings.cs
+++ src/Core/AppSettings.cs
@@ -11,7 +11,7 @@ public static class AppSettings
     public const string AppName         = "FluxTranslator";
     public const string AppPublisher    = "PawelKawka";
     public const string AppBaseVersion  = "1.4.0";
-    public const string AppBuild        = "2x05202601";
+    public const string AppBuild        = "2605202601";
     public const string AppVersion      = AppBaseVersion + "." + AppBuild;
     public const int    SttPort         = 5001;
 
