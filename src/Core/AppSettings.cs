88c79a786da7d61aa9ced88fa315de00a7e5067e
Dese193 <pawelk193@hotmail.com>
2026-05-28 20:56:06 +0200

    update AppSettings

--- src/Core/AppSettings.cs
+++ src/Core/AppSettings.cs
@@ -9,7 +9,7 @@ public enum TranslationEngine
 public static class AppSettings
 {
     public const string AppName         = "FluxTranslator";
-    public const string AppPublisher    = "PawelKawka";
+    public const string AppPublisher    = "desertseller";
     public const string AppBaseVersion  = "1.4.0";
     public const string AppBuild        = "2605202601";
     public const string AppVersion      = AppBaseVersion + "." + AppBuild;
